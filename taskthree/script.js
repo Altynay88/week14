089c843989fc1722929a03d466fcf1ac84194ac4
Altynay88 <79243168+Altynay88@users.noreply.github.com>
2021-09-17 17:14:13 +0600

    Add files via upload

---
+++ taskthree/script.js
@@ -0,0 +1,18 @@
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.btn-prev').addEventListener('click', function () {
+    offset = offset + 600;
+    if (offset > 1800) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.btn-next').addEventListener('click', function () {
+    offset = offset - 600;
+    if (offset < 0) {
+        offset = 1800;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
